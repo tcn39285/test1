5dabf998284cd07c13a7fd296ae9dc39d4ba3323
tcn39285 <86564586+tcn39285@users.noreply.github.com>
2021-06-27 11:05:14 -0700

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld{
   public static void main(String args[]){
-    System.out.println("Hello World from foler Program2"); 
+    System.out.println("Hello World from foler Program12"); 
   }
 }
  
